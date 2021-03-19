89ee5e71ace00f9411db59b0b2e136c43165d4fb
thymargo <thymargo@gmail.com>
2021-03-19 01:36:03 +0000

    [ADD] Pick-up module

---
+++ pick_up/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'Pick Up',
+    'summary':"""App to manage Pick-up spot""",
+    'description':"""
+        Pick Up Module to manage Pick-up:
+        - Pick-up method 
+            - Pick-up point
+            - pick up at store
+    """,
+    'category': 'Inventory/Delivery',
+    'version': '1.0',
+    'application': True,
+    'depends': ['base'],
+    'author': "Margo Tang",
+    'data': [
+	    
+    ],
+    'demo':[
+        
+    ],
+}
