3ff6607cc9d017716fe205a78a97a3a4857e1a56
Margo <32199948+thymargo13@users.noreply.github.com>
2021-05-14 12:13:37 +0800

    [ADD] app voip_sip_webrtc_twilio

---
+++ voip_sip_webrtc_twilio/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Voip Communication - Twilio",
+    'version': "1.0.25",
+    'author': "Sythil Tech",
+    'category': "Tools",
+    'summary': "Add support for Twilio XML",
+    'license':'LGPL-3',
+    'data': [
+        'views/voip_number_views.xml',
+        'views/voip_twilio_views.xml',
+        'views/voip_call_views.xml',
+        'views/voip_twilio_invoice_views.xml',
+        'views/res_partner_views.xml',
+        'views/res_users_views.xml',
+        'views/crm_lead_views.xml',
+        'views/voip_call_wizard_views.xml',
+        'views/voip_account_action_views.xml',
+        'views/voip_sip_webrtc_twilio_templates.xml',
+        'views/voip_call_comment_views.xml',
+        'views/mail_activity_views.xml',
+        'views/menus.xml',
+        #'data/voip.account.action.type.csv',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'depends': ['voip_sip_webrtc', 'account'],
+    'images':[
+    ],
+    'installable': True,
+}
